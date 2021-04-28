2778a71ad4dd83d4c86fb27694163481d8b0a308
dhairyamehta59 <dhairyamehta59@gmail.com>
2021-04-28 16:07:23 +0530

    cart assignment

---
+++ project/index.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+// Include functions and connect to the database using PDO MySQL
+include 'function.php';
+$pdo = pdo_connect_mysql();
+
+// Page is set to home (home.php) by default.
+$page = isset($_GET['page']) && file_exists($_GET['page'] . '.php') ? $_GET['page'] : 'home';
+
+include $page . '.php';
+?>
